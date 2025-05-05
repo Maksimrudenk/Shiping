6197da1b3c5a6e1f2460f75fd5f9185bf0d9a168
Maksim <MaksimRudenkoSc50@gmail.com>
2025-05-05 16:35:12 +0300

    start

---
+++ src/main/java/org/study/shiping/ShipingApplication.java
@@ -0,0 +1,13 @@
+package org.study.shiping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/study/shiping/ShipingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.study.shiping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
